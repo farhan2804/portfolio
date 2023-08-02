484ac47c4ae13e857783364058c0b37a7259c5b1
farhan <el3317.farhanmahmood@gmail.com>
2023-08-02 21:14:17 +0530

    modified files commit4

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import L_HTML5 from "../../assets/html-5.svg";
+import L_HTML5 from "../../assets/Images/Skills/html-5.svg";
 import Container from 'react-bootstrap/Container';
-import L_CSS3 from "../../assets/css3.svg";
-import L_BOOTSTRAP from "../../assets/bootstrap-4.svg";
+import L_CSS3 from "../../assets/Images/Skills/css3.svg";
+import L_BOOTSTRAP from "../../assets/Images/Skills/bootstrap-4.svg";
+import L_JAVASCRIPT from "../../assets/Images/Skills/javascript.svg";
+import L_REACT from "../../assets/Images/Skills/react.svg";
+import L_REACT_ROUTER from "../../assets/Images/Skills/react-router.svg";
+import L_REACT_BOOTSTRAP from "../../assets/Images/Skills/react-bootstrap.svg";
+import L_JAVA from "../../assets/Images/Skills/java.svg";
+import L_GIT from "../../assets/Images/Skills/git-icon.svg";
+import L_GITHUB_PAGES from "../../assets/Images/Skills/github.svg";
+import L_Heroku from "../../assets/Images/Skills/heroku.svg";
+import L_Netlify from "../../assets/Images/Skills/netlify.png";
 import './Skills.scss';
+
 
 const PortfolioSkills = () => {
     return (
@@ -14,46 +24,94 @@ const PortfolioSkills = () => {
             <hr class="w-50 mx-auto pt-3"></hr>
             <div class="row text-center mb-5">
                 <div class="col-lg-4 col-md-4 col-sm-12 col-12" id="Title1">
-                    <div id="SkillsContainer">
+                    <div id="SkillsContainer1">
                         <Card.Body>
-                            <h2 id="TitleHeading">Frontend</h2>
-                            <hr id="TitleLine"></hr>
-                            <div id="CardContents">
-                                <div class="HTMLImage">
+                            <h2 id="TitleHeading1">Frontend</h2>
+                            <hr id="TitleLine1"></hr>
+                            <div id="CardContents1">
+                                <a class="HTMLImage" href="https://en.wikipedia.org/wiki/HTML5" target="_blank" alt="image not loaded">
                                     <img src={L_HTML5} class="img-fluid" />
-                                    <h4>HTML5</h4>
-                                </div>
-                                <div class="CSSImage">
+                                    <h5 class="HTMLHeading">HTML5</h5>
+                                </a>
+                                <a class="CSSImage" href="https://medium.com/beginners-guide-to-mobile-web-development/whats-new-in-css-3-dcd7fa6122e1" target="_blank" alt="image not loaded">
                                     <img src={L_CSS3} class="img-fluid" />
-                                    <h4>CSS3</h4>
-                                </div>
-                                <div class="BootStrapImage">
+                                    <h5 class="CSSHeading">CSS3</h5>
+                                </a>
+                                <a class="BootStrapImage" href="https://getbootstrap.com/" target="_blank" alt="image not loaded">
                                     <img src={L_BOOTSTRAP} class="img-fluid" />
-                                    <h4>BootStrap5</h4>
-                                </div>
+                                    <h5 class="BootstrapHeading">Bootstrap5</h5>
+                                </a>
+                                <a class="JavaScriptImage1" href="https://www.javascript.com/" target="_blank" alt="image not loaded">
+                                    <img src={L_JAVASCRIPT} class="img-fluid" />
+                                    <h5 class="JavaScriptHeading">JavaScript</h5>
+                                </a>
+                                <a class="ReactImage" href="https://reactjs.org/" target="_blank" alt="image not loaded">
+                                    <img src={L_REACT} class="img-fluid" />
+                                    <h5 class="ReactHeading">React&nbsp;JS</h5>
+                                </a>
+                                <a class="ReactRouterImage" href="https://reacttraining.com/react-router/" target="_blank" alt="image not loaded">
+                                    <img src={L_REACT_ROUTER} class="img-fluid" />
+                                    <h5 class="ReactRouterHeading">React&nbsp;Router</h5>
+                                </a>
+                                <a class="ReactBootStrapImage" href="https://react-bootstrap.github.io/" target="_blank" alt="image not loaded">
+                                    <img src={L_REACT_BOOTSTRAP} class="img-fluid" />
+                                    <h5 class="ReactBootStrapHeading">React&nbsp;Bootstrap</h5>
+                                </a>
                             </div>
                         </Card.Body>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-4 col-sm-12 col-12">
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                        <Button id="ProjectButton" variant="primary">Go somewhere</Button>
-                    </Card.Body>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-12" id="title2">
+                    <div id="SkillsContainer2">
+                        <Card.Body>
+                            <h2 id="TitleHeading2">Programming <p>Languages</p></h2>
+                            <hr id="TitleLine2"></hr>
+                            <div id="CardContents2">
+                                <a class="JavaScriptImage2" href="https://www.javascript.com/" target="_blank" alt="image not loaded">
+                                    <img src={L_JAVASCRIPT} class="img-fluid" />
+                                    <h5 class="JavaScriptHeading">JavaScript</h5>
+                                </a>
+                                <a class="JavaImage" href="https://www.w3schools.com/java/" target="_blank" alt="image not loaded">
+                                    <img src={L_JAVA} class="img-fluid" />
+                                    <h5 class="JavaHeading">Java</h5>
+                                </a>
+                            </div>
+                        </Card.Body>
+                    </div>
+                    <div id="SkillsContainer3">
+                        <Card.Body>
+                            <h2 id="TitleHeading3">Version Control</h2>
+                            <hr id="TitleLine3"></hr>
+                            <div id="CardContents2">
+                                <a class="GITImage" href="https://git-scm.com/" target="_blank" alt="image not loaded">
+                                    <img src={L_GIT} class="img-fluid" />
+                                    <h5 class="GITHeading">GIT</h5>
+                                </a>
+                            </div>
+                        </Card.Body>
+                    </div>
                 </div>
-                <div class="col-lg-4 col-md-4 col-sm-12 col-12">
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                        <Button id="ProjectButton" variant="primary">Go somewhere</Button>
-                    </Card.Body>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-12" id="title3">
+                    <div id="SkillsContainer4">
+                        <Card.Body>
+                            <h2 id="TitleHeading4">Hosting Platforms</h2>
+                            <hr id="TitleLine4"></hr>
+                            <div id="CardContents3">
+                                <a class="HerokuImage" href="https://www.heroku.com/" target="_blank" alt="image not loaded">
+                                    <img src={L_Heroku} class="img-fluid" />
+                                    <h5 class="HerokuHeading">Heroku</h5>
+                                </a>
+                                <a class="GitHubImage" href="https://pages.github.com/" target="_blank" alt="image not loaded">
+                                    <img src={L_GITHUB_PAGES} class="img-fluid" />
+                                    <h5 class="GitHubHeading">GitHub Pages</h5>
+                                </a>
+                                <a class="NetlifyImage" href="https://www.netlify.com/" target="_blank" alt="image not loaded">
+                                    <img src={L_Netlify} class="img-fluid" />
+                                    <h5 class="NetlifyHeading">Netlify</h5>
+                                </a>
+                            </div>
+                        </Card.Body>
+                    </div>
                 </div>
             </div>
         </Container>
